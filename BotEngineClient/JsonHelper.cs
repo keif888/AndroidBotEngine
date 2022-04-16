0af0d36063fe74e8f416311cc4b19f5d74386724
Keith Martin <keith.martin@outlook.com.au>
2022-04-16 18:27:19 +1000

    Start adding json file validation capability with meaningful error messages.  And associated unit tests.

---
+++ BotEngineClient/JsonHelper.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+namespace BotEngineClient
+{
+    public class JsonHelper
+    {
+        public List<string> Errors;
+
+        public JsonHelper()
+        {
+            Errors = new List<string>();
+        }
+
+        public bool ValidateGameConfig(string jsonGameFileName, string jsonListFileName)
+        {
+            if (!ValidateListConfig(jsonListFileName))
+                return false;
+            return true;
+        }
+
+        public bool ValidateListConfig(string jsonListFileName)
+        {
+            JsonDocumentOptions documentOptions = new JsonDocumentOptions
+            {
+                AllowTrailingCommas = true,
+                CommentHandling = JsonCommentHandling.Skip
+            };
+            JsonNodeOptions nodeOptions = new JsonNodeOptions
+            {
+                PropertyNameCaseInsensitive = false
+            };
+
+            string jsonList = File.ReadAllText(jsonListFileName);
+            JsonNode jsonListNode = JsonNode.Parse(jsonList, nodeOptions, documentOptions);
+            if (jsonListNode is JsonObject)
+            {
+                JsonObject jsonObject = (JsonObject)jsonListNode;
+                if (!jsonObject.ContainsKey("FileId"))
+                {
+                    Errors.Add("Required field \"FileId\" missing.  Unable to confirm that the input file is a ListConfigFile");
+                }
+                else
+                {
+                    JsonNode fileIdValue = jsonObject["FileId"];
+                    if (fileIdValue is JsonValue)
+                    {
+                        JsonValue jsonValue = fileIdValue.AsValue();
+                        JsonElement value = fileIdValue.GetValue<JsonElement>();
+                        if (value.ValueKind == JsonValueKind.String)
+                        {
+                            string fileId = value.GetString();
+                            if (fileId.ToLower() != "listconfig")
+                            {
+                                Errors.Add(string.Format("\"FileId\" indicates that this is not \"ListConfig\" but {0}", fileId));
+                            }
+                        }
+                        else
+                        {
+                            Errors.Add(string.Format("\"FileId\" value is of incorrect type.  Expecting a String but got {0}", value.ValueKind));
+                        }
+                    }
+                    else
+                    {
+                        Errors.Add("Required field \"FileId\" is of the wrong type.  Expecting a String Value");
+                    }
+                }
+
+                if (!jsonObject.ContainsKey("Coordinates"))
+                {
+                    Errors.Add("Required field \"Coordinates\" missing.");
+                }
+                else
+                {
+                    JsonNode coordinatesValue = jsonObject["Coordinates"];
+                }
+            }
+            else
+            {
+                Errors.Add(string.Format("Root of {0} was not a json Object, was expecting {\"FileId\": \"ListConfig\",", jsonListFileName));
+            }
+            JsonNode root = jsonListNode.Root;
+            if (Errors.Count == 0)
+                return true;
+            else
+                return false;
+        }
+
+        public bool ValidateDeviceConfig(string jsonDeviceFileName, string jsonGameFileName, string jsonListFileName)
+        {
+            if (!ValidateGameConfig(jsonGameFileName, jsonListFileName))
+                return false;
+            return true;
+        }
+    }
+}
